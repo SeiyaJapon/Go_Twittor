cd1b1723e3d006c40ac31abd8bca406d28798ad0
Fran <francisco.perez@bolttech.io>
2023-05-13 14:16:14 +0200

    condicionales

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/SeiyaJapon/golang/go_twittor/variables"
 )
@@ -12,4 +13,19 @@ func main() {
 	estado, texto := variables.ConviertoATexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
+
+	os := runtime.GOOS
+
+	if os == "Linux." || os == "darwin" {
+		fmt.Println("Esto no es Windows")
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os {
+	case "Windows":
+		fmt.Println("Esto es Windows")
+	default:
+		fmt.Printf("Esto es %s", os)
+	}
 }
