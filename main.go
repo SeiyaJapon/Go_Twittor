fa23bb66924366c4af56f82383478ff076ad2aca
Fran <francisco.perez@bolttech.io>
2023-05-13 14:40:44 +0200

    ejercicio01b

--- main.go
+++ main.go
@@ -7,7 +7,19 @@ import (
 )
 
 func main() {
-	fmt.Println(ejercicios.ConvertToString("99"))
-	fmt.Println(ejercicios.ConvertToString("100"))
-	fmt.Println(ejercicios.ConvertToString("101"))
+	number, text := ejercicios.ConvertToString("99")
+	fmt.Println(number)
+	fmt.Println(text)
+
+	number2, text2 := ejercicios.ConvertToString("100")
+	fmt.Println(number2)
+	fmt.Println(text2)
+
+	number3, text3 := ejercicios.ConvertToString("101")
+	fmt.Println(number3)
+	fmt.Println(text3)
+
+	number4, text4 := ejercicios.ConvertToString("asdas")
+	fmt.Println(number4)
+	fmt.Println(text4)
 }
